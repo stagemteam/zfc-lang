ef33ff124c4355e2604b83eaf9f7e956bd90437d
popovsergiy <popow.sergiy@gmail.com>
2018-07-23 16:40:22 +0300

    Lang module has been improved

--- src/LangHelper.php
+++ src/LangHelper.php
@@ -14,14 +14,28 @@ class LangHelper
     /** @var CurrentHelper */
     protected $currentHelper;
 
+    /** @var Lang */
+    protected $currentLang;
+
     public function __construct(LangService $langService, CurrentHelper $currentHelper)
     {
         $this->langService = $langService;
         $this->currentHelper = $currentHelper;
     }
 
-    public function current()
+    /**
+     * @return Lang
+     */
+    public function getCurrentLang()
     {
-        return $this->langService->getCurrent();
+        return $this->currentLang;
+    }
+
+    /**
+     * @param Lang $currentLang
+     */
+    public function setCurrentLang($currentLang)
+    {
+        $this->currentLang = $currentLang;
     }
 }
